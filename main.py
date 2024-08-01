88ff06be75163f9fc5ff1e0773539127b7fa9577
Kamil <2217847@brunel.ac.uk>
2024-08-01 13:45:04 +0100

    Start of Element-X spin-off game.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # other machines might not.
 import pygame
 from os.path import join
+from random import randint
 
 # default pygame setup
 pygame.init()
@@ -15,27 +16,35 @@ clock = pygame.time.Clock()
 running = True
 
 # testing surface
-surf = pygame.Surface((100, 200))
-surfx, surfy = 100, 150
+player_pos = pygame.Surface((100, 200))
+# playerx, playery = 100, 150
 
 # image loading - loads all images used for the game, using the join method we can find the desired image
 # making the code more robust overall.
-player_ship = pygame.image.load(join('images', 'player.png')).convert_alpha()
+# if an image has no transparent pixels we want to call .convert otherwise .convert_alpha, increases fps, runs smoother
 
+player_ship = pygame.image.load(join('images', 'player.png')).convert_alpha()
+player_rect = player_ship.get_rect(center=(screenx / 2, screeny / 2))
+
+game_background = pygame.image.load(join('images', 'background.png')).convert()
+star_background = pygame.image.load(join('images', 'star.png')).convert_alpha()
+star_loc = [(randint(0, screenx), randint(0, screeny)) for i in range(20)]
 # ensures that code stays running forever unless the user closes out of the window
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked x to close the window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     # fill the screen with colour to wipe away anything from last frame
     screen.fill("Gray")
 
     # render game here
-    screen.blit(player_ship, (surfx, surfy))
-    surfx += 0.1
+    screen.blit(game_background, (0, 0))
+    for loc in star_loc:
+        screen.blit(star_background, loc)
+    screen.blit(player_ship, player_rect)
     # update() the display to put game on screen .flip works here as well
     pygame.display.update()
 
