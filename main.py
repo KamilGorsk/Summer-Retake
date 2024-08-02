9270877383774c6484023f8483c1b283b3cefcbf
Kamil <2217847@brunel.ac.uk>
2024-08-02 18:02:38 +0100

    Organising code into classes/sprites.

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 # Importing pygame, personal pycharm has it installed but when exporting the script,
 # other machines might not.
-import pygame
 from os.path import join
 from random import randint
-import time
+
+import pygame
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load(join('images', 'player.png')).convert_alpha()
+        self.rect = self.image.get_rect(center=(screenx / 2, screeny / 2))
+
 
 # default pygame setup
 pygame.init()
@@ -20,12 +28,14 @@ running = True
 # making the code more robust overall.
 # if an image has no transparent pixels we want to call .convert otherwise .convert_alpha, increases fps, runs smoother
 
-player_ship = pygame.image.load(join('images', 'player.png')).convert_alpha()
-player_rect = player_ship.get_rect(center=(screenx / 2, screeny / 2))
+player = Player
+
+# player_ship = pygame.image.load(join('images', 'player.png')).convert_alpha()
+# player_rect = player_ship.get_rect(center=(screenx / 2, screeny / 2))
 # using vector math for movement as vectors are incredibly powerful
-player_dir = pygame.math.Vector2(0, 0)
+# player_dir = pygame.math.Vector2(0, 0)
 # pixels/frame
-player_speed = 300
+# player_speed = 300
 
 game_background = pygame.image.load(join('images', 'background.png')).convert()
 
@@ -37,8 +47,6 @@ asteroid_rect = asteroid_main.get_rect(center=(screenx / 2, screeny / 2))
 
 projectile_main = pygame.image.load(join('images', 'projectile.png')).convert_alpha()
 projectile_rect = projectile_main.get_rect(bottomleft=(20, screeny - 20))
-
-# classes
 
 # ensures that code stays running forever unless the user closes out of the window
 
@@ -52,17 +60,17 @@ while running:
     # pygame.key / pygame.mouse are better for input than event loop as they make it easier to integrate with classes
     # also can check for continuous presses
     # input - storing return value inside a var for arrow keys + WASD movement using a boolean value
-    keys = pygame.key.get_pressed()
-    player_dir.x = int(keys[pygame.K_RIGHT] or keys[pygame.K_d]) - int(keys[pygame.K_LEFT] or keys[pygame.K_a])
-    player_dir.y = int(keys[pygame.K_DOWN] or keys[pygame.K_s]) - int(keys[pygame.K_UP] or keys[pygame.K_w])
+    # keys = pygame.key.get_pressed()
+    # player_dir.x = int(keys[pygame.K_RIGHT] or keys[pygame.K_d]) - int(keys[pygame.K_LEFT] or keys[pygame.K_a])
+    # player_dir.y = int(keys[pygame.K_DOWN] or keys[pygame.K_s]) - int(keys[pygame.K_UP] or keys[pygame.K_w])
     # normalizing movement, so it's consistent and stays at 300 whichever direction the player is going
-    player_dir = player_dir.normalize() if player_dir else player_dir
-    player_rect.center += player_dir * player_speed * dt
+    # player_dir = player_dir.normalize() if player_dir else player_dir
+    # player_rect.center += player_dir * player_speed * dt
 
     # projectile firing
-    recent_keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE]:
-        print('fire')
+    # recent_keys = pygame.key.get_pressed()
+    # if keys[pygame.K_SPACE]:
+    # print('fire')
     # fill the screen with colour to wipe away anything from last frame
     screen.fill("Gray")
 
@@ -72,7 +80,8 @@ while running:
         screen.blit(star_background, loc)
     screen.blit(projectile_main, projectile_rect)
 
-    screen.blit(player_ship, player_rect)
+    # screen.blit(player_ship, player_rect)
+    screen.blit(player.image, player.rect)
 
     screen.blit(asteroid_main, asteroid_rect)
     # update() the display to put game on screen .flip works here as well
