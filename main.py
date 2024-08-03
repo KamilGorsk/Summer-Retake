9cc61320beaa569b59d854e2b5b5712afbde254a
Kamil <2217847@brunel.ac.uk>
2024-08-03 15:33:49 +0100

    Organising into sprites and classes

--- main.py
+++ main.py
@@ -7,13 +7,31 @@ import pygame
 
 
 # sprites and classes
+# player class, already re-sized the asset in a separate program, I will not be needing a transform.scale
+# using vector math for movement as vectors are incredibly powerful, dir short for direction.
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, groups):
+        super().__init__(groups)
         self.image = pygame.image.load(join('images', 'player.png')).convert_alpha()
         self.rect = self.image.get_rect(center=(screenx / 2, screeny / 2))
         self.speed = 300
+        self.dir = pygame.math.Vector2()
 
+    # pygame.key / pygame.mouse are better for input than event loop as they make it easier to integrate with classes
+    # also can check for continuous presses
+    # input - storing return value inside a var for arrow keys + WASD movement using a boolean value
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        self.dir.x = int(keys[pygame.K_RIGHT] or keys[pygame.K_d]) - int(keys[pygame.K_LEFT] or keys[pygame.K_a])
+        self.dir.y = int(keys[pygame.K_DOWN] or keys[pygame.K_s]) - int(keys[pygame.K_UP] or keys[pygame.K_w])
+        # normalizing movement, so it's consistent and stays at 300 whichever direction the player is going
+        self.dir = self.dir.normalize() if self.dir else self.dir
+        self.rect.center += self.dir * self.speed * dt
+
+        # projectile firing
+        recent_keys = pygame.key.get_pressed()
+        if recent_keys[pygame.K_SPACE]:
+            print('fire')
 
 # default pygame setup
 pygame.init()
@@ -28,19 +46,10 @@ running = True
 
 # image loading - loads all images used for the game, using the join method we can find the desired image
 # making the code more robust overall.
-# if an image has no transparent pixels we want to call .convert otherwise .convert_alpha, increases fps, runs smoother
-
-# player_ship = pygame.image.load(join('images', 'player.png')).convert_alpha()
-# player_rect = player_ship.get_rect(center=(screenx / 2, screeny / 2))
-
-
-# Player class as I already re-sized the asset in a separate program, I will not be needing a transform.scale
-
-player = Player()
-# using vector math for movement as vectors are incredibly powerful
-# player_dir = pygame.math.Vector2(0, 0)
-# pixels/frame
-# player_speed = 300
+# if an image has no transparent pixels we want to use .convert otherwise .convert_alpha, increases fps, runs smoother
+# creating a group for all sprites for better optimization
+all_sprites = pygame.sprite.Group()
+player = Player(all_sprites)
 
 game_background = pygame.image.load(join('images', 'background.png')).convert()
 
@@ -62,20 +71,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    # pygame.key / pygame.mouse are better for input than event loop as they make it easier to integrate with classes
-    # also can check for continuous presses
-    # input - storing return value inside a var for arrow keys + WASD movement using a boolean value
-    # keys = pygame.key.get_pressed()
-    # player_dir.x = int(keys[pygame.K_RIGHT] or keys[pygame.K_d]) - int(keys[pygame.K_LEFT] or keys[pygame.K_a])
-    # player_dir.y = int(keys[pygame.K_DOWN] or keys[pygame.K_s]) - int(keys[pygame.K_UP] or keys[pygame.K_w])
-    # normalizing movement, so it's consistent and stays at 300 whichever direction the player is going
-    # player_dir = player_dir.normalize() if player_dir else player_dir
-    # player_rect.center += player_dir * player_speed * dt
 
-    # projectile firing
-    # recent_keys = pygame.key.get_pressed()
-    # if keys[pygame.K_SPACE]:
-    # print('fire')
+    # calls an update method on all the sprites inside the group, passing delta time through it
+    all_sprites.update(dt)
+
     # fill the screen with colour to wipe away anything from last frame
     screen.fill("Gray")
 
@@ -83,12 +82,12 @@ while running:
     screen.blit(game_background, (0, 0))
     for loc in star_loc:
         screen.blit(star_background, loc)
+
     screen.blit(projectile_main, projectile_rect)
-
-    # screen.blit(player_ship, player_rect)
-    screen.blit(player.image, player.rect)
-
     screen.blit(asteroid_main, asteroid_rect)
+    # draws every image in the group
+    all_sprites.draw(screen)
+
     # update() the display to put game on screen .flip works here as well
     pygame.display.update()
 
